c2adc6765246104a5a7b2c0a493591c4bf8a15d7
OpaqueGlass <opaqueglass@126.com>
2024-09-06 16:38:15 +0800

    fix. Add config of hotkey 添加快捷键设置

---
+++ src/hotkey.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core'
+import { HotkeyDescription, HotkeyProvider } from 'terminus-core'
+
+/** @hidden */
+@Injectable()
+export class QuickCmdsHotkeyProvider extends HotkeyProvider {
+    hotkeys: HotkeyDescription[] = [
+        {
+            id: 'quick-cmds.show',
+            name:'Open quick cmds panel',
+        },
+    ]
+
+    constructor (
+    ) { super() }
+
+    async provide (): Promise<HotkeyDescription[]> {
+        return [
+            ...this.hotkeys
+        ]
+    }
+}
